90faf6ee298d87459bc88ef6c89c9f16f4727665
Stanislav <AnisimovSA@apheco.ru>
2024-04-17 19:30:04 +0400

    added some code pretty

--- main.go
+++ main.go
@@ -7,22 +7,9 @@ import (
 	"time"
 )
 
-type Player struct {
-	name string
-}
-
-func (p *Player) Play(hit chan<- GameMsg) {
-	rand.Seed(time.Now().UnixNano())
-	if rand.Intn(100) <= 20 {
-		hit <- GameMsg{p, strongHit}
-	} else {
-		hit <- GameMsg{p, regularHit}
-	}
-}
-
 type GameMsg struct {
-	p   *Player
-	hit string
+	name string
+	hit  string
 }
 
 type GameScore struct {
@@ -30,8 +17,8 @@ type GameScore struct {
 	p2 int
 }
 
-func (s *GameScore) Add(p1 Player, msg GameMsg) {
-	if msg.p.name == p1.name {
+func (s *GameScore) Add(name string, msg GameMsg) {
+	if msg.name == name {
 		s.p1 += 1
 	} else {
 		s.p2 += 1
@@ -40,76 +27,80 @@ func (s *GameScore) Add(p1 Player, msg GameMsg) {
 
 const (
 	regularHit = "удар"
-	strongHit  = "Сильный удар"
+	strongHit  = "сильный удар!"
 )
 
 func main() {
-
-	fmt.Println("Игра в пинг-понг")
-	var p1Name string
-	fmt.Println("Введите имя первого игрока?")
-	fmt.Scan(&p1Name)
-
-	var p2Name string
-	fmt.Println("Введите имя второго игрока?")
-	fmt.Scan(&p2Name)
-
-	var finalScore int
-	fmt.Println("До скольки очков играем?")
-	fmt.Scan(&finalScore)
-	fmt.Println("Начинаем!")
-
-	p1 := Player{p1Name}
-	p2 := Player{p2Name}
+	p1Name, p2Name, finalScore := preparingToGame()
 	s := GameScore{0, 0}
 	game := make(chan GameMsg, 2)
 	done := make(chan struct{})
-
 	var wg sync.WaitGroup
-
 	for s.p1 < finalScore && s.p2 < finalScore {
 		wg.Add(2)
-
 		go func() {
 			defer wg.Done()
-			p1.Play(game)
+			Play(game, p1Name)
 		}()
-
 		go func() {
 			defer wg.Done()
-			p2.Play(game)
+			Play(game, p2Name)
 		}()
-
 		select {
 		case msg := <-game:
 			if msg.hit == strongHit {
-				fmt.Println(msg.p.name, msg.hit)
-				s.Add(p1, msg)
-				fmt.Println("Игрок ", msg.p.name, " получил +1 очко!")
+				fmt.Println(msg.name, msg.hit)
+				s.Add(p1Name, msg)
+				fmt.Println("Игрок ", msg.name, " получил +1 очко!")
 				fmt.Println("Текущий счет: ", s.p1, " : ", s.p2)
 				fmt.Println("Продолжаем!")
 			} else {
-				fmt.Println(msg.p.name, msg.hit)
+				fmt.Println(msg.name, msg.hit)
 			}
 			time.Sleep(time.Millisecond * 500)
 		case <-done:
-			fmt.Println("мы в done")
 			wg.Wait()
 			return
 		}
 	}
-
-	fmt.Println("Игра окончена!")
-	fmt.Println("Итоговый счет: ", s.p1, " : ", s.p2)
-	fmt.Println("Победитель: ", Winner(s, p1, p2).name)
-
+	printGameOver(s, p1Name, p2Name)
 }
 
-func Winner(s GameScore, p1, p2 Player) Player {
-	if s.p1 > s.p2 {
-		return p1
+func preparingToGame() (string, string, int) {
+	var pn1, pn2 string
+	var fs int
+	fmt.Println("Игра в пинг-понг")
+	fmt.Println("Введите имя первого игрока?")
+	fmt.Scan(&pn1)
+
+	fmt.Println("Введите имя второго игрока?")
+	fmt.Scan(&pn2)
+
+	fmt.Println("До скольки очков играем?")
+	fmt.Scan(&fs)
+	fmt.Println("Начинаем!")
+
+	return pn1, pn2, fs
+}
+
+func Play(hit chan<- GameMsg, name string) {
+	rand.Seed(time.Now().UnixNano())
+	if rand.Intn(100) <= 20 {
+		hit <- GameMsg{name, strongHit}
 	} else {
-		return p2
+		hit <- GameMsg{name, regularHit}
 	}
+}
+
+func printGameOver(s GameScore, p1, p2 string) {
+	fmt.Println("Игра окончена!")
+	fmt.Println("Итоговый счет: ", s.p1, " : ", s.p2)
+	var winner string
+	if s.p1 > s.p2 {
+		winner = p1
+	} else {
+		winner = p2
+	}
+	fmt.Println("Победитель: ", winner)
 
 }
